445b511011623778cfc9a60fecd1d96ffae8f524
Dominik Moritz <domoritz@gmail.com>
2013-03-13 18:26:50 +0100

    Log in example job

--- example/jobs.py
+++ example/jobs.py
@@ -1,9 +1,14 @@
+import logging
+
 import ckanserviceprovider.job as job
 import ckanserviceprovider.util as util
+
+logging.basicConfig()
 
 
 @job.sync
 def echo(task_id, input):
+    logging.warning('foo')
     if input['data'].startswith('>'):
         raise util.JobError('do not start message with >')
     if input['data'].startswith('#'):
