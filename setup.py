673a738fe2defc2f1246d22af7336d0f3150873b
amercader <amercadero@gmail.com>
2020-03-30 23:58:02 +0200

    Readme is markdown

--- setup.py
+++ setup.py
@@ -36,6 +36,7 @@ setup(
 
     description="A library for making web services that make functions available as synchronous or asynchronous jobs",
     long_description=long_description,
+    long_description_content_type='text/markdown',
 
     # The project's main homepage.
     url='https://github.com/ckan/ckan-service-provider',
