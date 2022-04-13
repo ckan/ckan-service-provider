9e2c246a3a06c8e50d0d80d6eceaa9484c544e69
amercader <amercadero@gmail.com>
2022-04-13 12:56:39 +0200

    Remove test line

--- tests/test_joberror.py
+++ tests/test_joberror.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 
-import sys
 
 from ckanserviceprovider.util import JobError
 
@@ -8,7 +7,3 @@ from ckanserviceprovider.util import JobError
 def test_joberror():
     err = JobError("oh no ❌")
     assert err.message == "oh no ❌"
-    if sys.version_info[0] == 2:
-        assert unicode(err) == "oh no ❌"
-    else:
-        assert str(err) == "oh no ❌"
